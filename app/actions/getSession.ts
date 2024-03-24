e84aa6bec3a3896fcf960f63005a20a5ee10bb71
Yvan jr Blanchette <yvanblanchette@gmail.com>
2024-03-24 08:01:26 -0400

    fix

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../api/auth/[...nextauth]/route';
+import { authOptions } from '@/app/libs/authOptions';
 
 // Function to get the session for the user...
 export default async function getSession() {
--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from 'next-auth';
 import { pusherServer } from '@/app/libs/pusher';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/app/libs/authOptions';
 
 // This is the handler for the /api/pusher/auth route
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
